b4e412c2d5f1c167850d2c4eaf52a4f18a2d0838
Stan <lol97554@gmail.com>
2024-01-31 00:43:32 +0900

    feat: acceptNetworks & tappay args in info.plist

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,14 +1,45 @@
-import { ConfigPlugin, withEntitlementsPlist } from "expo/config-plugins";
+import {
+  ConfigPlugin,
+  withEntitlementsPlist,
+  withInfoPlist,
+} from "expo/config-plugins";
 
-const withApplePayEntitlement: ConfigPlugin<{
+interface ConfigPayload {
   merchantId: string;
-}> = (config, { merchantId }) => {
-  return withEntitlementsPlist(config, (config) => {
+  acceptNetworks: "Visa" | "MasterCard" | "JCB" | "AmEx"[];
+  appId: number;
+  appKey: string;
+  serverType: "sandbox" | "production";
+}
+
+const withApplePayEntitlement: ConfigPlugin<ConfigPayload> = (
+  config,
+  { merchantId, acceptNetworks, appId, appKey, serverType },
+) => {
+  config = withEntitlementsPlist(config, (config) => {
     console.log("Applying withApplePayEntitlement...");
     config.modResults["com.apple.developer.in-app-payments"] = [merchantId];
 
     return config;
   });
+
+  config = withInfoPlist(config, (config) => {
+    console.log("Applying withTappayConfig...");
+    config.modResults["TPDApplePayPaymentNetworks"] = acceptNetworks;
+    config.modResults["TPDAppId"] = appId;
+    config.modResults["TPDAppKey"] = appKey;
+
+    if (serverType !== "sandbox" && serverType !== "production") {
+      throw new Error(
+        "Invalid serverType. serverType must be either 'sandbox' or 'production'",
+      );
+    }
+
+    config.modResults["TPDServerType"] = serverType;
+    return config;
+  });
+
+  return config;
 };
 
 module.exports = withApplePayEntitlement;
