5f35df6fdd32a2aa7dce5daa94126740bdfbc3e4
Stan <lol97554@gmail.com>
2024-05-06 19:43:34 +0800

    chore: update example application.

--- example/App.tsx
+++ example/App.tsx
@@ -1,52 +1,43 @@
-import * as ExpoTappayApplePay from "expo-tappay-apple-pay";
-import { useRef } from "react";
+import applePay from "expo-tappay-apple-pay";
 import { Button, StyleSheet, View } from "react-native";
 
 export default function App() {
-  const applePay = useRef<ExpoTappayApplePay.ApplePay>(null);
+  applePay.addReceivePrimeListener((event) => {
+    if (event.success) {
+      applePay.showResult(true);
+    }
+  });
 
   const setupMerchant = () => {
-    applePay.current?.setupMerchant(
-      "Halfway Studio",
-      "3DS",
-      "merchant.lava.hwstud.io",
-      "TW",
-      "TWD",
-    );
+    applePay.setupMerchant({
+      name: "Tappay Test",
+      capabilities: "3DS",
+      merchantId: "merchant.tech.cherri.global.test",
+      countryCode: "TW",
+      currencyCode: "TWD",
+    });
   };
 
-  const addItem = () => {
-    applePay.current?.addItemToCart("test", 100);
+  const showSetup = () => {
+    applePay.showSetup();
+  };
+
+  const startPayment = () => {
+    applePay.startApplePay({
+      cart: [
+        {
+          name: "Test",
+          amount: 100,
+        },
+      ],
+    });
   };
 
   return (
     <View style={styles.container}>
       <Button title="Setup Merchant" onPress={setupMerchant} />
-      <Button title="Add Item" onPress={addItem} />
-      <ExpoTappayApplePay.ApplePay
-        ref={applePay}
-        style={{
-          width: "40%",
-        }}
-        onApplePayStart={() => {
-          console.log("onApplePayStart");
-        }}
-        onReceivePrime={(event) => {
-          console.log("onReceivePrime", event);
-        }}
-        onApplePaySuccess={(event) => {
-          console.log("onApplePaySuccess", event);
-        }}
-        onApplePayCancel={() => {
-          console.log("onApplePayCancel");
-        }}
-        onApplePayFailed={(event) => {
-          console.log("onApplePayFailed", event);
-        }}
-        onApplePayFinished={() => {
-          console.log("onApplePayFinished");
-        }}
-      />
+      <Button title="Show Setup" onPress={showSetup} />
+      <Button title="Start Payment" onPress={startPayment} />
     </View>
   );
 }
